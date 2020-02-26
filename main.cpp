761463dce8fbbc534d47193b2664bfc30d5386b1
Thai Truong <thaitruong@Thais-MacBook-Pro.local>
2020-02-25 21:16:26 -0800

    Update logic of round winner and add condition for game end

--- main.cpp
+++ main.cpp
@@ -1,38 +1,54 @@
 #include <iostream>
-#include <vector>
 #include <string>
 #include <cstdlib>
 
 using namespace std;
-class Player {
-    private:
-        enum choice{
+enum OPTION{
             ROCK, PAPER, SCISSOR, NO_CHOICE
         };
+class Player {
+    private:
+        OPTION choice;
     public:
-        void set__choice();
+        void set__choice(OPTION user_choice);
+        OPTION get__choice();
 };
 
 class Game {
     private:
+        bool is_Over;
         const int ROUNDS;
         int round_count;
         Player human_player,machine_player;
         int human_result,machine_result;
     public:
         Game();
+        bool is__draw(); // check if it is draw or not 
         void set__round_count(int round_th);
         int get__round_count();
         void set_result(Player winner);
         int get__human_result();
         int get__machine_result();
-        void is_roundWinner();  // Most important logic of the program
+        Player is_roundWinner();  // Most important logic of the program
         void is_gameWinner();
-        void start_game(); // clear choice after each round
+        void start_round(); // clear choice after each round
 };
 
 int main(){
     Game();
+}
+bool Game::is__draw(){
+    bool is_draw = FALSE;
+    if(machine_player.get__choice() == human_player.get__choice()){
+        is_draw = TRUE;
+    }
+    return is_draw;
+}
+OPTION Player::get__choice(){
+    return choice;
+}
+void Player::set__choice(OPTION user_choice){
+    choice = user_choice;
 }
 void Game::set__round_count(int round_t){
     round_count = round_t;
@@ -66,4 +82,15 @@ void Game::is_gameWinner(){
 
 int Game::get__round_count(){
     return round_count;
+}
+
+Player Game::is_roundWinner() {
+    Player winner;
+    if((human_player.get__choice() == ROCK) && (machine_player.get__choice() == SCISSOR) || 
+    (human_player.get__choice() == PAPER) && (machine_player.get__choice() == ROCK) ||
+    (human_player.get__choice() == SCISSOR) && (machine_player.get__choice == PAPER))
+    {
+        winner = human_player;
+    } else winner = machine_player;
+    return winner;
 }
