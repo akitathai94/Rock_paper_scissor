f70c5adadfab620219149096fa978bdfe79c8b19
Riya <jriya1998@gmail.com>
2020-02-27 21:55:30 -0800

    Updated functions and terminal works for game

--- SimpleVersion/Game.cpp
+++ SimpleVersion/Game.cpp
@@ -5,6 +5,7 @@ Game :: Game(int total_rounds){
     user_wins_count = 0;
     computer_wins_count = 0;
     current_round_no = 0;
+    srand(time(0));
 }
 
 int Game :: getRoundWinner(OPTIONS userChoice)
@@ -12,7 +13,8 @@ int Game :: getRoundWinner(OPTIONS userChoice)
     // -1 if computer won
     //  0 if draw
     //  1 if user won
-    int x = compareChoices(getComputerChoice(), userChoice);
+    current_round_computer_choice = getComputerChoice();
+    int x = compareChoices(current_round_computer_choice, userChoice);
     switch (x)
     {
         case -1:    computer_wins_count++;
@@ -41,12 +43,28 @@ int Game :: getGameWinner()
 OPTIONS Game :: getComputerChoice()
 {
     // return the choice made by computer
-    return OPTIONS::ROCK;
+    return static_cast<OPTIONS>(rand()%3);
 }
 int Game :: compareChoices(const OPTIONS choice1, const OPTIONS choice2)
 {
     // -1 if choice1 is winner
     // 0 if draw
     // 1 if choice2 is winner
+    switch(choice1)
+    {
+        case OPTIONS::ROCK :    if(choice2 == OPTIONS::SCISSOR) return -1;
+                                else if(choice2==OPTIONS::PAPER) return 1;
+                                else return 0;
+                                break;
+        case OPTIONS::SCISSOR : if(choice2 == OPTIONS::PAPER) return -1;
+                                else if(choice2==OPTIONS::ROCK) return 1;
+                                else return 0;
+                                break;                      
+        case OPTIONS::PAPER : if(choice2 == OPTIONS::ROCK) return -1;
+                                else if(choice2==OPTIONS::SCISSOR) return 1;
+                                else return 0;
+                                break; 
+            
+    }
     return 0;
 }
--- SimpleVersion/Game.h
+++ SimpleVersion/Game.h
@@ -2,6 +2,8 @@
 #define GAME_RPS
 
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 #include "OPTIONS.h"
 using namespace std;
 class Game{
@@ -11,12 +13,14 @@ class Game{
     int current_round_no;               // Round that is currently going on
     OPTIONS getComputerChoice();            // Logic to generate computer choice
     int compareChoices(const OPTIONS , const OPTIONS);            // return the winner of two choices
-    
+    OPTIONS current_round_computer_choice;
     public:
     
     Game(int );                         // Constructor
     int getRoundWinner(OPTIONS );       // returns winner based on user input
     int getGameWinner();                // returns the winner of final game
+    OPTIONS getCurrentRoundComputerChoice() {return current_round_computer_choice;};
+    int getCurrentRoundNo() {return current_round_no;};
 };
 
 #endif
